75b3301d50089d642acbbfbb914e0a5b2b5eec55
David Sanders <dsanders@rapilabs.com>
2017-03-14 17:05:42 +1100

    Added dead simple drag-n-drop using html5 dnd
     - Used a wrapper to make dragenter/leave behave like mouseenter/leave by Melbourne's very own Ben Smithett
     - Kept simple with xhr post + page refresh, no client-side state

--- kanban/urls.py
+++ kanban/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from .views import index
+from .views import drop, index
 
 urlpatterns = [
+    url(r'^drop/$', drop),
     url(r'^$', index),
 ]
--- kanban/views.py
+++ kanban/views.py
@@ -1,9 +1,25 @@
+import json
+
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import ensure_csrf_cookie
 
-from .models import Board
+from .models import Board, Card, Column
 
 
+@ensure_csrf_cookie
 def index(request):
     return render(request, template_name='kanban/base.html', context={
         'boards': Board.objects.all(),
     })
+
+
+def drop(request):
+    payload = json.loads(request.body)
+    card_id = int(payload.get('card_id'))
+    column_id = int(payload.get('column_id'))
+    assert card_id and column_id
+    card = Card.objects.get(id=card_id)
+    card.column = Column.objects.get(id=column_id)
+    card.save()
+    return HttpResponse()
